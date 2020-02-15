31efaceecd374119f7dfb641f213a877964f25f3
nl <nl@nl.com>
2020-02-15 08:28:18 -0600

    handle json null serialization & func refactor

--- src/main/kotlin/com/myretail/service/domain/RedSky.kt
+++ src/main/kotlin/com/myretail/service/domain/RedSky.kt
@@ -1,9 +1,12 @@
 package com.myretail.service.domain
 
-data class RedSky(val product: RedSkyProduct)
+import com.fasterxml.jackson.annotation.JsonInclude
+
+data class RedSkyResponse(val product: RedSkyProduct)
 
 data class RedSkyProduct(val item: RedSkyProductItem)
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class RedSkyProductItem(val product_description: RedSkyProductItemDesc?)
 
 data class RedSkyProductItemDesc(val title: String)
--- src/main/kotlin/com/myretail/service/handler/ProductHandler.kt
+++ src/main/kotlin/com/myretail/service/handler/ProductHandler.kt
@@ -1,20 +1,18 @@
 package com.myretail.service.handler
 
 import com.myretail.service.domain.ErrorMsg
-import com.myretail.service.domain.RedSky
+import com.myretail.service.domain.RedSkyResponse
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.badRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
-import java.util.function.IntPredicate
 
 @Service
 class ProductHandler {
@@ -22,26 +20,22 @@ class ProductHandler {
     private val host = "https://redsky.target.com"
     private val webClient: WebClient = WebClient.create(host)
 
-    fun retrieveProductInfo(request: ServerRequest): Mono<ServerResponse> {
-        val uri = "/v2/pdp/tcin/${request.pathVariable("id")}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics"
-
-        return Flux
-                .interval(Duration.ofMillis(200))
-                .flatMap {
-                    webClient
-                            .get()
-                            .uri(uri)
-                            .retrieve()
-                            .onStatus(HttpStatus::is4xxClientError) { Mono.empty() }
-                            .onStatus(HttpStatus::is5xxServerError) { Mono.error<Throwable>(Throwable("redsky not available")) }
-                            .bodyToMono<RedSky>()
-                }
-                .retryBackoff(3, Duration.ofMillis(100))
-                .take(1)
-                .flatMap { ok().body<RedSky>(Mono.just(it)) }
-                .last()
-                .onErrorResume(::errorResponse)
-    }
+    fun retrieveProductInfo(request: ServerRequest) = Flux
+            .interval(Duration.ofMillis(200))
+            .flatMap {
+                webClient
+                        .get()
+                        .uri("/v2/pdp/tcin/${request.pathVariable("id")}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics")
+                        .retrieve()
+                        .onStatus(HttpStatus::is4xxClientError) { Mono.empty() }
+                        .onStatus(HttpStatus::is5xxServerError) { Mono.error<Throwable>(Throwable("redsky not available")) }
+                        .bodyToMono<RedSkyResponse>()
+            }
+            .retryBackoff(3, Duration.ofMillis(100))
+            .take(1)
+            .flatMap { ok().body<RedSkyResponse>(Mono.just(it)) }
+            .last()
+            .onErrorResume(::errorResponse)
 }
 
 private fun errorResponse(throwable: Throwable) = badRequest().body<ErrorMsg>(Mono.just(ErrorMsg(throwable.message)))
