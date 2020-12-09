0764f4a1227f79a710f2689de82baf7dbddb2f51
nl <nl@nl.com>
2020-12-08 20:49:04 -0800

    removing logging

--- src/main/kotlin/com/myretail/service/service/RedSkyService.kt
+++ src/main/kotlin/com/myretail/service/service/RedSkyService.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.flow.single
-import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
@@ -16,17 +15,13 @@ import org.springframework.web.reactive.function.client.awaitBody
 @Service
 class RedSkyService(@Value("\${redsky.host:https://redsky.target.com}") private val host: String) {
     private val webClient: WebClient = WebClient.create(host)
-    private val logger = KotlinLogging.logger {  }
 
     suspend fun getProductTitle(id: Int) = flow { emit(invokeRedSkyCall(id)) }
             .retry(3) { e -> (e is Exception).also { if (it) delay(50) } }
             .catch { emit(redSkyError()) }
             .single()
 
-    suspend fun invokeRedSkyCall(id: Int): RedSkyResponse {
-        logger.info { "Invoking red sky call" }
-        return webClient.get().uri(rsUri(id)).retrieve().awaitBody<RedSkyResponse>()
-    }
+    suspend fun invokeRedSkyCall(id: Int) = webClient.get().uri(rsUri(id)).retrieve().awaitBody<RedSkyResponse>()
 
     private fun rsUri(id: Int) = "/v3/pdp/tcin/$id?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate"
 
